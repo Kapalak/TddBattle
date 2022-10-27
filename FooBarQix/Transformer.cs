91c4530a0232ab9dd2c9d604fdfab6f98d077069
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-10-27 14:10:48 +0200

    Fix Test

--- FooBarQix/Transformer.cs
+++ FooBarQix/Transformer.cs
@@ -4,16 +4,40 @@
     {
         public string Compute(int input)
         {
+            string result = string.Empty;
             if (input % 3 == 0)
             {
-                return "FooFoo";
+                result += "FooFoo";
             }
             else if (input.ToString().Contains("3"))
             {
-                return "Foo";
+                result += "Foo";
             }
+            if (input % 3 == 0 && input.ToString().Contains("5") && input % 5 == 0)
+            {
+                result = "FooBarBar";
+            }
+            else if (input.ToString().Contains("5") && input % 3 == 0)
+            {
+                result = "FooBar";
+            }
+            else if (input.ToString().Contains("5") && input % 5 == 0)
+            {
+                result = "BarBar";
+            }
+            else if (input.ToString().Contains("5"))
+            {
+                result += "Bar";
+            }
+            else if (input % 5 == 0)
+            {
+                result += "Bar";
+            }
+            
 
-            return input.ToString();
+            if (result == string.Empty)
+                return input.ToString();
+            return result;
         }
     }
 }
