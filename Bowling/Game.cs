16f57b55d0f16943a7efc5ca84f35c3dc1e93ead
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-10-27 12:32:20 +0200

    clean game

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -2,17 +2,8 @@
 {
     public class Game
     {
-        private int[] rolls = new int[21];
-        int currentRoll = 0;
-        private bool isSpare(int frameIndex)
-        {
-            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
-        }
-
-        private bool isStrike(int frameIndex)
-        {
-            return rolls[frameIndex] == 10;
-        }
+        private readonly int[] rolls = new int[21];
+        private int currentRoll;
 
         public void Roll(int pins)
         {
@@ -27,39 +18,9 @@
             }
         }
 
-        //public int Score()
-        //{
-        //    var r = rolls;
-        //    var f = frames;
-        //    return 0;
-        //    //throw new NotImplementedException();
-        //}
-
         public int Score()
         {
-            int score = 0;
-            int frameIndex = 0;
-            for (int frame = 0; frame < 10; frame++)
-            {
-                if (isSpare(frameIndex))
-                {
-                    score += 10 + rolls[frameIndex + 2];
-                    frameIndex += 2;
-                }
-                else if (isStrike(frameIndex))
-                {
-                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
-                    frameIndex++;
-                }
-                else
-                {
-                    score += rolls[frameIndex] + rolls[frameIndex + 1];
-                    frameIndex += 2;
-                }
-
-            }
-
-            return score;
+            throw new NotImplementedException();
         }
     }
 }
