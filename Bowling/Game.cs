f795fb87915185367d633cc78c8a1ad083119b8a
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-10-27 10:40:08 +0200

    add bowling game

---
+++ Bowling/Game.cs
@@ -0,0 +1,65 @@
+﻿namespace Bowling
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        int currentRoll = 0;
+        private bool isSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        private bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
+        }
+
+        //public int Score()
+        //{
+        //    var r = rolls;
+        //    var f = frames;
+        //    return 0;
+        //    //throw new NotImplementedException();
+        //}
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (isStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+
+            }
+
+            return score;
+        }
+    }
+}
